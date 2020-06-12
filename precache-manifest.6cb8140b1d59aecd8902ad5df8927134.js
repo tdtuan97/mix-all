3e380ce65aa43e5e33dd8a6e424279707f393119
tdtuan97 <tdtuan97@gmail.com>
2020-06-12 12:13:34 +0700

    Updates

---
+++ precache-manifest.6cb8140b1d59aecd8902ad5df8927134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a061d1509cc324e4cf9b",
+    "url": "/redux-react/static/css/main.1119e413.chunk.css"
+  },
+  {
+    "revision": "a061d1509cc324e4cf9b",
+    "url": "/redux-react/static/js/main.a061d150.chunk.js"
+  },
+  {
+    "revision": "2afa80718802b47e8ef0",
+    "url": "/redux-react/static/js/1.2afa8071.chunk.js"
+  },
+  {
+    "revision": "f7792b0326b3ed44d9e7",
+    "url": "/redux-react/static/js/runtime~main.f7792b03.js"
+  },
+  {
+    "revision": "5c11e8dba5b3d2dfee139094345c46c1",
+    "url": "/redux-react/static/media/rekit-react.5c11e8db.png"
+  },
+  {
+    "revision": "3cf7c79153c8796147f964c2d80f648d",
+    "url": "/redux-react/index.html"
+  }
+];
