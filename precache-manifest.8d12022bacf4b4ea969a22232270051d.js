bc497c4a7369c3e046d5e5ba0c3e920f3d209b0e
tdtuan97 <tdtuan97@gmail.com>
2020-06-14 16:28:57 +0700

    Updates

---
+++ precache-manifest.8d12022bacf4b4ea969a22232270051d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9eeb2de52da36627f43",
+    "url": "/redux-react/static/css/main.853a750b.chunk.css"
+  },
+  {
+    "revision": "f9eeb2de52da36627f43",
+    "url": "/redux-react/static/js/main.f9eeb2de.chunk.js"
+  },
+  {
+    "revision": "3adadedb6c8e9c5dcb0f",
+    "url": "/redux-react/static/js/1.3adadedb.chunk.js"
+  },
+  {
+    "revision": "f7792b0326b3ed44d9e7",
+    "url": "/redux-react/static/js/runtime~main.f7792b03.js"
+  },
+  {
+    "revision": "65f270d7d57999a5f48b8855d4a181b4",
+    "url": "/redux-react/static/media/logo.65f270d7.png"
+  },
+  {
+    "revision": "7dfac08d683b81d4755c4f28ae936f2e",
+    "url": "/redux-react/static/media/banner_background.7dfac08d.jpg"
+  },
+  {
+    "revision": "f1dd3d620d35452a6e4d12af1e3fe98e",
+    "url": "/redux-react/static/media/banner_title.f1dd3d62.png"
+  },
+  {
+    "revision": "65f270d7d57999a5f48b8855d4a181b4",
+    "url": "/redux-react/static/media/player.65f270d7.png"
+  },
+  {
+    "revision": "79906b22bcd706710ebc569b50ee182b",
+    "url": "/redux-react/static/media/background.79906b22.jpg"
+  },
+  {
+    "revision": "546f3f7dfcb688fcc682d1c7191fdc93",
+    "url": "/redux-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/redux-react/precache-manifest.a34256c39f18af9b884847e6e8c78fa9.js"
+  "/redux-react/precache-manifest.8d12022bacf4b4ea969a22232270051d.js"
 );
 
 workbox.clientsClaim();
